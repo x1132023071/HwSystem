f7dd1a9f660cfd928609a358aaa194d6dc43d235
x1132023071 <1132023071@qq.com>
2018-05-13 19:57:40 +0800

    初步设计

---
+++ WorkS/WorkS/login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WorkS
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(radioButton2.Checked){
+            mainSt mst = new mainSt();
+            this.Visible = false;
+            mst.ShowDialog();
+            radioButton1.Checked = true;
+            textBox2.Text = "";
+            this.Visible = true;
+            }
+            else if(radioButton1.Checked){
+                mainTe mte = new mainTe();
+                this.Visible = false;
+                mte.ShowDialog();
+                radioButton1.Checked = true;
+                textBox2.Text = "";
+                this.Visible = true;
+            }
+        }
+
+    }
+}
